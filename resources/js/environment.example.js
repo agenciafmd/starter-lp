094ee67becea050922451c3e783b2ff8f4dc37ee
edmo.souza <edmo@fmd.ag>
2020-07-14 16:25:43 -0300

    feat(performance): Adiciona critical-path

--- resources/js/environment.example.js
+++ resources/js/environment.example.js
@@ -1,5 +1,15 @@
-module.exports = (function() {
+function environment() {
+
   return {
-    domain: 'starter-lp.local'
+    production: false,
+    domain: 'starter-lp.local',
   };
-})();
+}
+
+try {
+
+  module.exports = (environment)();
+} catch (e) {
+
+  console.warn('Module is not supported, please refactor it!');
+}
