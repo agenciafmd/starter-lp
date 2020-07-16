d442f0487e5401067f3ed248ae5b22ef5e8686df
edmo.souza <edmo@fmd.ag>
2020-07-16 17:03:36 -0300

    feat(environment): Adiciona post-prod
    - Como frontender precisamos de um centralizador de informacao para que
    ao executar um comando fosse alterado o conteudo de alguns items
    que esta na /public/index.html. A ideia teve como base o env que possui
    no starter-laravel

--- resources/js/environment.example.js
+++ resources/js/environment.example.js
@@ -3,6 +3,15 @@ function environment() {
   return {
     production: false,
     domain: 'starter-lp.local',
+    postStrings: {
+      titlePage: 'Starter-LP F&MD',
+      metaDescription: 'Descrição completa da página',
+      ogSiteName: 'Starter-LP | Nome do cliente',
+      ogTitle: 'Starter-LP | Nome do cliente',
+      ogDescription: 'Descrição curta para aparecer no compartilhar',
+      ogUrlProduction: 'https://www.starterlp.com.br',
+      gtmCode: 'GTM-',
+    },
   };
 }
 
