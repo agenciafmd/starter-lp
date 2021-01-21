42ab568948dba693282da7fd13e082f894f2686d
Milena <milena.ramiro@fmd.ag>
2021-01-21 17:47:04 -0300

    Nova variavel adicionada no arquivo environment.example e IF para verificar ambiente no webpack. PREPARANDO TERRENO PARA MIGRACAO NET CORE

--- webpack.mix.js
+++ webpack.mix.js
@@ -164,3 +164,14 @@ mix.browserSync({
     interval: 500,
   },
 });
+
+if(environment.folder){
+  mix
+      .copyDirectory('public/css', environment.folder + '/wwwroot/css')
+      .copyDirectory('public/fonts', environment.folder + '/wwwroot/fonts')
+      .copyDirectory('public/images',environment.folder + '/wwwroot/images')
+      .copyDirectory('public/js', environment.folder + '/wwwroot/js')
+      .copyDirectory('public/pdf',environment.folder + '/wwwroot/pdf')
+      .copyDirectory('public/svg',environment.folder + '/wwwroot/svg')
+      .copy('public/favicon.ico', environment.folder + '/wwwroot/favicon.ico');
+}
