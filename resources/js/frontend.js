af34f41fc0c87b219b2ad487f35bc66c5dc2b5e9
Renan Breno <renan.breno@fmd.ag>
2021-03-08 17:27:01 -0300

    feat(fmd header): Adiciona FMD header ao starter-lp

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -542,7 +542,7 @@ window.addEventListener('load', function () {
    * We need the starting function here because vh/vw are calculated after all
    * resources loaded, which is different from DOM ready event
    * */
-  // setupStickyHeader();
+  setupFmdHeader();
 
-  // setupInfiniteScroll();
+    // setupInfiniteScroll();
 });
