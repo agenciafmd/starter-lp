5f649a31090624989338472a23faa18345f505d6
Vitor Hugo F Lopes <vitor.automec@hotmail.com>
2022-03-03 23:30:17 -0300

    feat(disable submit): Adiciona captura botoes fora do formulario

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -112,9 +112,15 @@ function preventInvalidFormSubmit() {
 
 function disableButtonOnSubmit(form) {
 
-  const buttons = form.querySelectorAll('button');
+  const coupledFormButtons = form.querySelectorAll('button');
+  const uncoupledFormButtons = document.querySelectorAll(`button[form=${ form.id }]`);
 
-  buttons.forEach((button) => {
+  const foundFormButtons = [
+    ...coupledFormButtons,
+    ...uncoupledFormButtons,
+  ];
+
+  foundFormButtons.forEach((button) => {
 
     button.setAttribute('disabled', 'disabled');
 
